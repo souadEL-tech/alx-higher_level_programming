f83be89e7e5cac05382b0a52160026e219d45cc4
SOUAD_TECH <sou.elmarsani@gmail.com>
2023-11-29 09:47:46 -0800

    last dift of a number

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -9,5 +9,5 @@ if last_dig > 5:
     print(f"Last digit of {number:d }is {last_dig:d }and is greater than 5")
 elif last_dig == 0:
     print(f"Last digit of {number:d }is {last_dig:d } and is 0")
-elif last_dig < 6 and last_dig != 0:
+else
     print(f"Last digit of {number:d }is {last_dig:d }and is less than 6 and not 0")
