55f0973bb7074b3f260741f8f8c1d1cc5713cd37
SOUAD_TECH <sou.elmarsani@gmail.com>
2023-11-29 09:38:51 -0800

    last dift of a number

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,6 +3,8 @@ import random
 
 number = random.randint(-10000, 10000)
 last_dig = number % 10
+if number < 0:
+    last_dig = -last_dig
 if last_dig > 5:
     print(f"Last digit of {number }is {last_dig }and is greater than 5")
 elif last_dig == 0:
