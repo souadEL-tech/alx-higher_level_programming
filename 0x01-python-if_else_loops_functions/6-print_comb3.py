627f2fa404f9e240bebd7ac54a43e042df042c4a
SOUAD_TECH <sou.elmarsani@gmail.com>
2023-12-02 12:58:51 -0800

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -6,6 +6,6 @@ for i1 in range(0, 10):
         if i1 == i2:
             pass
         elif "{:d}{:d}".format(i1, i2) == '89':
-            print("{:d}{:d}".format(i1, i2))
+            print("{}{}".format(i1, i2))
         else:
-            print("{:d}{:d}".format(i1, i2), end="1 ")
+            print("{}{}".format(i1, i2), end="1 ")
