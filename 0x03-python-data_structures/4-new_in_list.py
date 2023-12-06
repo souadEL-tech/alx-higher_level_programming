ad546e2ed3dca2a7259433d51805024b9fe81035
SOUAD_TECH <sou.elmarsani@gmail.com>
2023-12-06 01:33:31 -0800

    4-new_in_list.py

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def new_in_list(my_list, idx, element):
+
+    if idx < 0:
+        return (my_list)
+
+    for i in range(len(my_list)):
+        if i == idx:
+            my_list.insert(i, element)
+            return (my_list)
+    return (my_list)
