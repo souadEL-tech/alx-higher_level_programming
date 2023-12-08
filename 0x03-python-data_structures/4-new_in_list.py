ffc56d0635707fc1ca7f1502386210e9242bc425
SOUAD_TECH <sou.elmarsani@gmail.com>
2023-12-08 02:10:07 -0800

    4-new_in_list.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -9,7 +9,7 @@ def new_in_list(my_list, idx, element):
 
     for i in range(len(my_list)):
         if i == idx:
-            new_list[i] = element
+            new_list.append(element)
         else:
-            new_list[i] = my_list[i]
+            new_list.append(my_list[i])
     return (new_list)
