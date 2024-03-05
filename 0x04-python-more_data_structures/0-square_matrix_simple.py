2e86bc8170267f7ab30281c69c709bb86c3a5690
SOUAD_TECH <sou.elmarsani@gmail.com>
2024-03-05 11:01:30 -0800

    0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -6,8 +6,11 @@ def square_number(number):
 
 def square_matrix_simple(matrix=[]):
     new_matrix = []
+    new_list = list()
     for col in matrix:
         for row in col:
             new_row = square_number(row)
-            new_matrix.append(new_row)
+            new_list.append(new_row)
+        new_matrix.append(new_list)
+        new_list= list()
     return (new_matrix)
