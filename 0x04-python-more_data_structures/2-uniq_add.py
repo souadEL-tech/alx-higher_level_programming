4479bc40fdc9ddaaa41187a1cfd1e7210a7d61b6
souad elma <sou.elmarsani@gmail.com>
2024-04-25 11:02:13 -0700

    2-uniq_add.py

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -4,7 +4,7 @@ def uniq_add(my_list=[]):
 
     uni_list = list()
     result = 0
-    
+
     uni_list.append(my_list[0])
     for i in my_list:
         if i not in uni_list:
@@ -12,5 +12,6 @@ def uniq_add(my_list=[]):
         else:
             pass
     for j in uni_list:
-        result = result +j
+        result = result + j
+
     return result
