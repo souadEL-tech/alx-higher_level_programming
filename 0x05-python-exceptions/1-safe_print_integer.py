ccab8165fe0d4bd961643c8bf4c3d743c833c431
souad elma <sou.elmarsani@gmail.com>
2024-05-19 11:07:13 -0700

    1-safe_print_integer.py

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def safe_print_integer(value):
+    # check if val is an integer
+
+    try:
+        if valeu == int(valeu):
+            print("{:d}".format(valeu))
+            return (True)
+    except Exception:
+        return (False)
