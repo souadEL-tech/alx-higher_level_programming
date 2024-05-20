b358d4f12c6c1542f1456ced27f036b438898ea1
souad elma <sou.elmarsani@gmail.com>
2024-05-20 05:09:03 -0700

    1-safe_print_integer.py

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,7 +3,7 @@
 def safe_print_integer(value):
     # check if val is an integer
     try:
-        if value == int(value):
+        if value/value == 1:
             print("{:d}".format(value))
     except Exception:
         return (False)
