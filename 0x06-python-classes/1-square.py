4a25c223db50263ac420f0c84e0c32e6db8f6510
souad elma <sou.elmarsani@gmail.com>
2024-05-26 11:12:21 -0700

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""class Square that defines a square by: (based on 0-square.py"""
+
+
+class Square:
+
+    """redefine suquare class
+    """
+
+    def __init__(self, __size):
+        """ constructor
+        args:
+        size : int private
+        """
+        self.__size = __size
