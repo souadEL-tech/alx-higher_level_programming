c1d5338e24f5a250be84fd852e71a7d05f7aacc5
souad elma <sou.elmarsani@gmail.com>
2024-05-30 11:06:42 -0700

    3-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+""" class Square that defines a square by: (based on 2-square.py)"""
+
+
+class Square:
+    """redefine suquare class
+    """
+
+    def __init__(self, size=0):
+
+        """ constructor
+        args:
+        size : int private with optional val
+        """
+        if size != int(size):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """
+        return the area of size
+        """
+        return self.__size ** 2
