075582ebf925b8f1065dcea2e49deaa3818f4614
souad elma <sou.elmarsani@gmail.com>
2024-07-03 11:10:00 -0700

    5-text_indentation.py tests/5-main.py

---
+++ 0x07-python-test_driven_development/tests/5-main.py
@@ -0,0 +1,4 @@
+!/usr/bin/python3
+text_indentation = __import__('5-text_indentation').text_indentation
+
+text_indentation("""Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quonam modo? Utrum igitur tibi litteram videor an totas paginas commovere?""")
