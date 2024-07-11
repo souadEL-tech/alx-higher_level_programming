0043205faf410360f1da93ee1a2c00bf0bb66398
souad elma <sou.elmarsani@gmail.com>
2024-07-11 03:56:51 -0700

    3-rectangle.py

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -90,8 +90,10 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return("")
         else:
+            result = []
             for i in range(self.__height):
-                for j in range(self.__width):
-                    print("#", end="")
-                print("")
-            return ('')
+                # List comprehension
+                [result.append('#')for j in range(self.__width)]
+                if i != self.__height - 1:
+                    result.append("\n")
+            return ("".join(result))
