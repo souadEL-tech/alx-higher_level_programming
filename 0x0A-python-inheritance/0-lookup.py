598723a3840bce86b42faacacfae620c215e0f28
souad elma <sou.elmarsani@gmail.com>
2024-07-26 16:31:47 -0700

    0-lookup.py

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
+""" Defines an object attribute lookup function."""
+
 
 def lookup(obj):
-
-    obj_list = list()
-    obj_list = dir(obj)
-    return (obj_list)
+    """ Return a list of an object's available attributes."""
+    return(dir(obj))
